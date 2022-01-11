7665307bb3254b84fa60fe57299f5922ef1506a1
Stranger N <91267135+adventurboy@users.noreply.github.com>
2022-01-11 11:27:49 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,13 +41,8 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🛡 Group ', url='https://t.me/SL_Movies_Group'),
+            InlineKeyboardButton('🎯Channel', url='https://t.me/SL_Linkz')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -82,13 +77,8 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🛡 Group ', url='https://t.me/SL_Movies_Group'),
+            InlineKeyboardButton('🎯Channel', url='https://t.me/SL_Linkz')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
